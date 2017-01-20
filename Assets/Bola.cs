fecd9dd490ad66ecf6e51f0f37ca9cc151d2543c
McOmghall <pmgberm@gmail.com>
2017-01-20 22:12:35 +0100

    Prototipo onda maleable con controles

---
+++ Assets/Bola.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour {
+  public float amplitude = 100;
+  public float hspeed = 60;
+  public float hspeedChangePerSecond = 5;
+  public float amplitudeChangePerSecond = 5;
+
+  void Update () {
+    hspeed += Input.GetAxis("Horizontal") * hspeedChangePerSecond * Time.deltaTime;
+    amplitude += Input.GetAxis("Vertical") * amplitudeChangePerSecond * Time.deltaTime;
+  }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+    transform.position = new Vector3(transform.position.x + hspeed * Time.deltaTime, amplitude * Mathf.Sin(Time.time), 0.0f);
+  }
+}
